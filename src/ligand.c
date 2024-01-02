0f5145e27e95593fb905e0a1f39e2410cbf20352
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-01-02 11:34:05 +0800

    Update ligand.c

--- src/ligand.c
+++ src/ligand.c
@@ -14,9 +14,9 @@ typedef struct {
 
 static PyTypeObject LigandType = {
     .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "custom.Custom",
-    .tp_doc = PyDoc_STR("Custom objects"),
-    .tp_basicsize = sizeof(CustomObject),
+    .tp_name = "ligand.Ligand",
+    .tp_doc = PyDoc_STR("Ligand objects"),
+    .tp_basicsize = sizeof(LigandObject),
     .tp_itemsize = 0,
     .tp_flags = Py_TPFLAGS_DEFAULT,
     .tp_new = PyType_GenericNew,
